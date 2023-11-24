e9f252e569999024fa45ce1d348b302ee06f9e23
Andreas Braun <git@alcaeus.org>
2023-11-24 12:33:52 +0100

    Use new Symfony Bundle for MongoDB

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    MongoDB\Bundle\MongoDBBundle::class => ['all' => true],
 ];
